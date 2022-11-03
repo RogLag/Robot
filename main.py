2956ef72e72a2f7705fd2c79b3a5412451ac621f
Hugo.M <hugo.rog@icloud.com>
2022-11-03 15:30:14 +0100

    add coroutines

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from machine import Pin
 import pycom
 from BME280 import *
-import time
 import asyncio
+import time
 
 async def capteur():
     while True :
@@ -15,6 +15,7 @@ async def capteur():
         print("l'humidité est de : ",z)
         await asyncio.sleep(5)
         print("\n")
+    return None
 
 print("Robot init :\n")
 
@@ -27,5 +28,10 @@ Id_BME280 = bus_i2c.readfrom_mem(BME280_I2C_ADR, BME280_CHIP_ID_ADDR, 1)
 print ('Valeur Id_BME280 : ', hex (Id_BME280[0]))
 capteur_BME280 = BME280 (BME280_I2C_ADR, bus_i2c)
 capteur_BME280.Calibration_Param_Load()
+
 print("\nRobot ready")
-capteur()
+
+temp = capteur()
+
+time.sleep(10)
+print("finish")
